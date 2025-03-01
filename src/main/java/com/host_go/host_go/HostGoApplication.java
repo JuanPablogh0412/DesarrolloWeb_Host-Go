75dbd13979f4016b2f13c7769837be82ebb02587
JuanPGH0412 <juanpablogh0412@gmail.com>
2025-03-01 17:51:53 -0500

    Creacion proyecto SpringBoot

---
+++ src/main/java/com/host_go/host_go/HostGoApplication.java
@@ -0,0 +1,13 @@
+package com.host_go.host_go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/host_go/host_go/HostGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.host_go.host_go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
